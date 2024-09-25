d6d2815d715c87418e46041077c812aba41bc22e
Misza2009 <162618047+Misza2009@users.noreply.github.com>
2024-09-25 13:18:58 +0200

    Liczby Palindromiczne

--- main.cpp
+++ main.cpp
@@ -2,12 +2,23 @@
 using namespace std;
 
 int main() {
-    int l1, l2;
+    int l1, l2,/* l3=l1*/;
     cin>> l1;
     while(l1>0)
     {
         l2=l2 * 10 + l1%10;
         l1= l1/10;
     }
-    cout<< l2;
+ /*   if (l3==l2){
+        cout<<"Tak";
+    }
+    else{
+        cout<<"Nie";
+    }*/
+    cout<<l2;
+
+    //if robi sie bez klamr, poprawić potem!!
+    // DO NAPRAWIENIA!!!!!!!!
+    // NIE DZIAŁA po usunięciu klamr
+
 }
